f16d78f1370c49db78f9223fcce79296fce143c6
Daniel Rojo Reyes <drojo6507@gmail.com>
2025-03-21 09:30:40 -0600

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from 'react'
+import { useState } from 'react'
+import "./css/style.css"
+
+const App = () => {
+  const [listaTareas, setListaTareas] = useState([]);
+  
+  return (
+    <>
+    <apiComponents/>
+   
+    </>
+    
+  )
+}
+
+export default App;
+
+
+//Rafc para funciones de flecha 
+//elimina la carpeta assets
+//elimina archivo app.css y el index.css
+//<></> retornar varios elementos sin poner div 
+//usestatesnippe 
+//para utilizar variables js se utiliza {}
+//preprocesador react npm run build
+//en las etiquetas de los componentes ctl + espacio para importar 
